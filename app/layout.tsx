590277f95a9b3963ff37e0e80461a58c5e0818b3
thejokers69 <thejokkers69@gmail.com>
2025-07-07 23:54:38 +0100

    Add @vercel/analytics for enhanced analytics tracking
    - Included @vercel/analytics dependency in package.json and pnpm-lock.yaml.
    - Imported and utilized Analytics component in app/layout.tsx for improved user insights.
    
    Signed-off-by: thejokers69 <thejokkers69@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
+import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -36,6 +38,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
